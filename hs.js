a03bfac4d78d39969c7ee1dc6c39a7bc78496254
oikarinen <oikarinen>
2016-12-02 00:08:45 +0200

    initial

---
+++ hs.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name       Paywall buster
+// @version    0.1
+// @match      http://www.hs.fi/*
+// @match      http://www.ksml.fi/*
+// @match      http://www.kauppalehti.fi/*
+// ==/UserScript==
+
+// hs.fi
+localStorage._hist = "plop";
+
+// kauppalehti.fi
+localStorage.__frosmoeasy__readArticles = "plop";
+
+// ksml.fi 
+localStorage.paywall_history = "plop";
